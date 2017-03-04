34a811f8d31d2910af65551481c2ef1d772d4c20
Nikhil Sanjay Wagh <nikhilwagh.work@gmail.com>
2017-03-04 08:43:27 +0530

    Update score.php

--- score.php
+++ score.php
@@ -1,23 +1,25 @@
 <?php
     include("session.php");
-
-function calmarks($conn,$selected)
+//error_reporting(0);
+//echo $_SESSION['qno'];
+function calmarks($conn2,$selected)
 {
-    $sql = "SELECT correct,cmarks,inmarks FROM questionbank.".$_SESSION['qbid']." WHERE qno = ".$_SESSION['qno'];
-    $result = mysqli_query($conn,$sql);
+    $sql = "SELECT correct,cmarks,inmarks FROM id593597_questionbank.".$_SESSION['qbid']." WHERE qno = ".$_SESSION['questions'][$_SESSION['qno']-1];
+    //echo $sql;
+    $result = mysqli_query($conn2,$sql);
     $row = mysqli_fetch_array($result);
     if($selected==$row['correct'])
     {
         $_SESSION['score']+=$row['cmarks'];
     }
-    else
+    else if($selected!=0)
     {
         $_SESSION['score']-=$row['inmarks'];
     }
 }
 function update($conn)
 {
-    $sql = "UPDATE quizzer.".$_SESSION['examid']." SET score = ".$_SESSION['score'].", attempted = ".$_SESSION['qno']." WHERE handle = '".$_SESSION['handle']."'";
+    $sql = "UPDATE id593597_quizzer.".$_SESSION['examid']." SET score = ".$_SESSION['score'].", attempted = ".($_SESSION['qno'])." WHERE handle = '".$_SESSION['handle']."'";
     //echo $sql;
     if(mysqli_query($conn,$sql))
     {
@@ -26,10 +28,11 @@ function update($conn)
 }
 if(isset($_POST['btn-next']))
 {
+    $_SESSION['qno']++;
     if(isset($_POST['radiobutton']))
     {
         $selected = $_POST['radiobutton'];
-        calmarks($conn,$selected);
+        calmarks($conn2,$selected);
        
     }
     update($conn);
@@ -39,11 +42,15 @@ if(isset($_POST['btn-next']))
 
 if(isset($_POST['btn-submit']))
 {
+    $_SESSION['qno']++;
     if(isset($_POST['radiobutton']))
     {
         $selected = $_POST['radiobutton'];
-        calmarks($conn,$selected);
-        echo "<script>alert(".$_SESSION['score'].")</script>";
+        calmarks($conn2,$selected);
+        ?>
+        <script type="text/javascript">
+        alert("Your final score is "+ '<?php echo $_SESSION['score'] ?>')</script>
+        <?php
     }
     update($conn);
 }
@@ -51,6 +58,7 @@ if(isset($_POST['btn-submit']))
 
 <html lang ="en">
 <head>
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
 <title>Test</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width,initial-scale=1"> 
@@ -58,23 +66,29 @@ if(isset($_POST['btn-submit']))
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <link rel="stylesheet" href="css/mdb.min.css">
-    <style>
-        .container{
-            padding: 10px;
-            margin: auto;
-            display: block;
-        }
-        .btn-toolbar{
-            margin: auto;
-            float: right;
-        
-            
-        }
-    </style>
+
 </head>
 <body>
     <form action="leader.php" method="post">
-    <button class="btn btn-dark-green waves-effect waves-light " type="submit"  name="btn-leader" id="btn-leader" >Leader Board</button>
+    <div class ="container" style ="height:20vh;">
+    <div class ="container" style="height: 60vh;">
+  <div class="row">
+
+		<div class="col-md-12">
+                
+	            <h4  style="text-align:center;">
+			Click the button below to see the leader board
+		     </h4>
+             
+
+		</div>
+
+	</div>
+        <div class="row">
+            <div class="col-md-12 flex-center">
+            <button class="btn btn-dark-green waves-effect waves-light " type="submit" name="btn-leader" id="btn-leader" >Leader Board</button>
+        </div>
+        </div>
     </form>
     </body>
 </html>
