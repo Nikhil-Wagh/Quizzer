032ce9e0756097495f4d1c48fb85452abd8eaee5
Nikhil Sanjay Wagh <nikhilwagh.work@gmail.com>
2017-03-04 08:43:55 +0530

    Update selectqb.php

--- selectqb.php
+++ selectqb.php
@@ -1,5 +1,6 @@
 <?php
 include("session.php");
+//error_reporting(0);
 if(!isset($_SESSION['handle']))
 {
     ?>
@@ -19,40 +20,65 @@ if(isset($_POST['btn-selectqb']))
 ?>
 <html lang ="en">
 <head>
-<title>Create your database</title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width,initial-scale=1"> 
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<link rel="stylesheet" href="css/mdb.min.css">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <title>Select Question Bank</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/mdb.min.css" rel="stylesheet">
 </head>
 <body>
 
-
-<form  method="post">
-    <div class="dropdown" >
-        <select class="form-control" name="qbid">  
-            <?php
-                $sql="SELECT * FROM qbdetails WHERE handle = '".$_SESSION['handle']."'";
-                $result=mysqli_query($conn,$sql);
-                if(mysqli_num_rows($result)>0)
-                {
-                    while($row=mysqli_fetch_array($result))
-                    {
-                        echo "<option value='".$row['id']."'>".$row['qbname']."</option>";
-                    }
-                }
-                else
-                {
-                    ?>
-                    <script>alert("Please create a question bank.");</script>
+<div class="container" style="height: 10vh;"></div>
+<div class="container flex-center" style="height: 10vh;">
+    <h1 class="display-4"> Select Question Bank </h1>
+</div>
+<div class="container" style="height: 20vh;"></div>
+<div class="container" style="height: 60vh;">
+    <form  method="post">
+        <div class="row">
+            <div class="col-sm-12">
+                <div>
+                    <h4 style="text-align: center;"><small> Add questions to the following question bank </small></h4>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="dropdown" style="width: 50%; margin: auto; display: block;">
+                <select class="form-control" name="qbid">  
                     <?php
-                }
-            ?>
-         </select>  
-    </div>
-    <button class="btn btn-danger waves-effect waves-light" type="submit"  name="btn-selectqb" id="btn-selectqb" >Select Question Bank</button>   
-</form>
-</body> 
+                        $sql="SELECT * FROM qbdetails WHERE handle = '".$_SESSION['handle']."'";
+                        $result=mysqli_query($conn,$sql);
+                        if(mysqli_num_rows($result)>0)
+                        {
+                            while($row=mysqli_fetch_array($result))
+                            {
+                                echo "<option value='".$row['id']."'>".$row['qbname']."</option>";
+                            }
+                        }
+                        else
+                        {
+                            ?>
+                            <script>alert("Please create a question bank.");</script>
+                            <?php
+                        }
+                    ?>
+                </select>  
+            </div>
+            <div class="col-md-4"></div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12">
+                <button class="btn btn-danger" type="submit"  name="btn-selectqb" id="btn-selectqb" style="margin: auto; display: block;">Enter</button>
+            </div> 
+        </div>
+    </form>
+</div>
+
+</body>
 </html>
